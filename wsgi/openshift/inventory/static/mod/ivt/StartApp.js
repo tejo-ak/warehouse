d07bf0f9d448a1d5d0aedf397b0f17fe185b821b
Tejo <tejo.ak@gmail.com>
2012-06-22 08:29:22 +0700

    nightly owl, anim menu pabean

--- wsgi/openshift/inventory/static/mod/ivt/StartApp.js
+++ wsgi/openshift/inventory/static/mod/ivt/StartApp.js
@@ -8,7 +8,8 @@
 define(['dojo',
     'dojo/parser',
     'lib/TabUtil',
-    'lib/dojote'
+    'lib/dojote',
+    'dijit/Tooltip'
 ], function (dojo, parser, tabUtil, dojote) {
 
     var singleton = {
@@ -25,7 +26,7 @@ define(['dojo',
         prepareHome:function (arg) {
             var arg = (arg) ? arg : {};
             if (!tabUtil.cekTab(this.formHome)) {
-                this.formHome = tabUtil.putinTab('Home', 'Loading home ...');
+                this.formHome = tabUtil.putinFirstTab('Home', 'Loading home ...');
                 dojote.callXhrPost('/inventory/', {c:'home'}, dojo.hitch(this, function (e) {
                     if (tabUtil.cekTab(this.formHome)) {
                         this.formHome.set('content', e)
@@ -33,6 +34,7 @@ define(['dojo',
                 }))
 
             } else {
+                tabUtil.closeAfterTab(this.formHome)
             }
         },
 
@@ -48,6 +50,7 @@ define(['dojo',
                 }))
 
             } else {
+                tabUtil.closeAfterTab(this.formPabean)
             }
         },
         buildPabean:function () {
@@ -57,13 +60,12 @@ define(['dojo',
                     var cn = dives[i].className;
                     if (cn.indexOf('BC') != -1) {
                         var dv = dives[i]
+                        dv.id = dojote.getUuid().substr(0, 6);
                         dojo.style(dv, 'cursor', 'pointer');
                         var img = dojo.query('img', dv)[0];
                         if (!this.formPabean['omov' + dv.className])
                             this.formPabean['omov' + dv.className] = dojo.connect(dv, 'onmouseover', dojo.hitch(this, function (e, dv) {
                                 var img = e;
-                                console.log(img);
-                                console.log(e);
                                 var imgsrc = img.src;
                                 if (imgsrc.indexOf('/images/') != -1) {
                                     var newimgsrc = imgsrc.replace('/images/', '/images_glow/');
@@ -73,8 +75,6 @@ define(['dojo',
                         if (!this.formPabean['omou' + dv.className])
                             this.formPabean['omov' + dv.className] = dojo.connect(dv, 'onmouseout', dojo.hitch(this, function (e, dv) {
                                 var img = e;
-                                console.log(img);
-                                console.log(e);
                                 var imgsrc = img.src;
                                 if (imgsrc.indexOf('/images_glow/') != -1) {
                                     var newimgsrc = imgsrc.replace('/images_glow/', '/images/');
@@ -86,7 +86,25 @@ define(['dojo',
                 }
 
             }
+            var tt30 = dojo.query('.BC30-dokumen-css', this.formPabean.domNode)[0];
+            var tt27in = dojo.query('.BC27IN-dokumen-css', this.formPabean.domNode)[0];
+            var tt27 = dojo.query('.BC27-dokumen-css', this.formPabean.domNode)[0];
+            console.log('tt30.id')
+            console.log(tt30.id)
+            var tt = new dijit.Tooltip({connectId:tt30.id, label:'<span class="siderLabel">' +
+                'Export declaration<br/>' +
+                'Dokumen deklarasi pabean untuk ekspor barang<br/>' +
+                'ke luar daerah pabean Indonesia</span>'})
+            var tt27in = new dijit.Tooltip({connectId:tt27in.id, label:'<span class="siderLabel">' +
+                'Movement Control<br/>' +
+                'Dokumen pelindung perpindahan barang antar TPB<br/>' +
+                ' </span>'})
+            var tt27 = new dijit.Tooltip({connectId:tt27.id, label:'<span class="siderLabel">' +
+                'Movement Control<br/>' +
+                'Dokumen pelindung perpindahan barang antar TPB<br/>' +
+                ' </span>'})
         }
+
     };
     singleton._init();
     return singleton;
